7816269e7c61cceba51741726cbf858e2cdaffc7
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2018-12-01 22:45:42 +0700

    update select and helper

---
+++ src/Fields/Select.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LiveCMS\Resources\Fields;
+
+class Select extends Field
+{
+    protected $options = [];
+    protected $usingLabel = false;
+
+    public function options(array $options)
+    {
+        $this->options = $options;
+        return $this;
+    }
+
+    public function displayUsingLabels($state = true)
+    {
+        $this->usingLabel = $state;
+        return $this;
+    }
+
+    public function toDatatable()
+    {
+        return [
+            $this->label => [
+                'name' => $this->field,
+                'orderable' => $this->usingLabel ? false : $this->sortable,
+                'resolve' => function ($row) {
+                    $this->setModel($row);
+                    $value = $this->value();
+                    return $this->usingLabel ? $this->options[$value] : $value;
+                },
+            ],
+        ];
+    }
+
+    public function toShow()
+    {
+        $value = $this->value();
+        return $this->usingLabel ? $this->options[$value] : $value;
+    }
+
+    public function toForm()
+    {
+        return [
+            'type' => 'select',
+            'label' => $this->getLabel(),
+            'options' => $this->options,
+        ];
+    }
+}
